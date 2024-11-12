43411ffb7d583b87d36690827feae3380912d81e
MrAzuka <johntwice8@gmail.com>
2024-11-12 02:20:17 +0100

    feat: initalize wallet resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,8 +5,9 @@ import { UserModule } from './user/user.module';
 import { ConfigModule } from '@nestjs/config';
 import { validate } from './common/util/environment.validation';
 import { AuthModule } from './auth/auth.module';
+import { WalletModule } from './wallet/wallet.module';
 
 @Module({
-  imports: [DatabaseModule, CommonModule, UserModule, ConfigModule.forRoot({ isGlobal: true, validate }), AuthModule]
+  imports: [DatabaseModule, CommonModule, UserModule, ConfigModule.forRoot({ isGlobal: true, validate }), AuthModule, WalletModule]
 })
 export class AppModule { }
